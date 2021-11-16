d369f326fd518f663bedf4b00271b7990d40ef87
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-11-15 16:57:31 -0800

    testing initial functions

---
+++ assets/js/game.js
@@ -0,0 +1,20 @@
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName 
+console.log(playerName);
+
+//Examples
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10+20);
+
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
